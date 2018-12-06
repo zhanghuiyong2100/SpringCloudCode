69038f85b91c836a1647df66db85317755bbad33
zhang <zhouyaqi@deppon.com>
2018-12-06 15:15:13 +0800

    1205:1.创建服务生产者eureka-hello项目;2.eureka-server项目提高高可用性

--- README.md
+++ README.md
@@ -8,5 +8,7 @@
     1.上传到GitHub,并新建README.MD文件，idea中安装Markdown Navigator便于编辑
     2.确定父工程管理子工程,创建父工程项目,经过对比,最后选择springBoot版本为2.0.7，springCloud版本为Finchley.SR2
     3.创建子项目eureka-server,作为服务注册中心
-
+2018-12-05
+    1.创建eureka-hello作为服务提供者（即生产者）
+    2.为了进一步的高可用性,在eureka-server项目中添加peer1和peer2的配置文件，并分别启动，以达到类似集群的效果
 ```
---
+++ eureka-hello/src/main/java/com/springcloud/eurekahello/web/restcontroller/HelloController.java
@@ -0,0 +1,37 @@
+package com.springcloud.eurekahello.web.restcontroller;
+
+import com.sun.istack.internal.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+/**
+ * @author 章辉勇
+ * @创建时间 2018年12月06日
+ * @描述: eureka服务hello接口:注册服务提供者
+ **/
+@RestController
+public class HelloController {
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() {
+        List<String> clientServices = client.getServices();
+        logger.info("hello接口" + clientServices +"端口号："+port);
+        return "This is Eureka,Hello";
+    }
+
+
+}
