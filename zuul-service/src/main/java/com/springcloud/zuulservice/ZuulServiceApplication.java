5dc9f00d55938586f645806d3c9bc3dbc605f034
zhang <zhouyaqi@deppon.com>
2018-12-14 09:05:00 +0800

    1213-1:1.创建路由网关项目zuul补充

---
+++ zuul-service/src/main/java/com/springcloud/zuulservice/ZuulServiceApplication.java
@@ -0,0 +1,21 @@
+package com.springcloud.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+/** 开启路由网关zuul的功能  */
+@EnableZuulProxy
+public class ZuulServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServiceApplication.class, args);
+    }
+
+}
+
